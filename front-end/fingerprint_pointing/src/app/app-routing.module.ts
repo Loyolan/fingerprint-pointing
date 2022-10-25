e0e4df0e8f34eb358b8bfe9fb2dc124e9d874404
Loyolan <kiadyyrann@gmail.com>
2022-10-25 23:23:20 +0300

    INTERFACE

--- front-end/fingerprint_pointing/src/app/app-routing.module.ts
+++ front-end/fingerprint_pointing/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'eni/figerprint_pointing/home', component: HomeComponent },
+  { path: '', redirectTo: 'eni/figerprint_pointing/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//  { path: '**', canActivate: [AuthGuard], component: FourOFourComponent }
--- front-end/fingerprint_pointing/src/app/app.module.ts
+++ front-end/fingerprint_pointing/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    FourOhFourComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
